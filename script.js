8f15cf63705e7ce2c59b7890d4d477ef2e8470af
himanshuy21 <93994213+himanshuy21@users.noreply.github.com>
2022-04-15 20:54:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+
+    // Validation of principal amount
+    if(principal == ''){
+        alert('Enter the principal amount, it cannot be blank');
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    if(principal<= 0){
+        alert("Enter the Positive no. Of Principal Amount")
+        document.getElementById("principal").focus();
+        return; 
+    }
+
+
+//rate value
+var rate = parseInt(document.getElementById("rate").value);
+//years value
+var years = parseInt(document.getElementById("years").value);
+
+    var year = new Date().getFullYear() + years;
+    // Simple Interest Formula 
+    let interest = (principal*years*rate)/100;
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+ interest +",\<br\>in the year "+year+"\<br\>"
+}
+function updateRate(){
+   let  rateval = document.getElementById("rate").value;
+   document.getElementById("rate_val").innerText= rateval +"%";
 }
         
